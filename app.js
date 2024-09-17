c8a53b63d5a807c08bbf222fbde31ddb4c36486a
Eziz8 <isayev_eziz@mail.ru>
2024-09-17 11:25:39 +0400

    commit

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+const check =(array,num)=>{
+array.map((indexOne)=>{
+array.map((indexTwo)=>{
+    if(indexOne+indexTwo===num){
+     console.log(true);
+     console.log(indexOne,indexTwo);
+    }
+})
+})
+}
+check([1,3,4,5,7,9],4)
+*/
+///////////////////////////////////////////////////////////
+/*
+let arr=[64, 34, 25, 12, 22, 11, 90]
+
+const ascend=(arr)=>{
+    console.log(arr.sort((a,b)=>a-b));
+}
+
+ascend(arr)
+*/
+
+/*
+const stringSort = array => {
+const arr = Array.from(array); 
+for (let i = 1; i < arr.length; i++) {
+for (let j = 0; j < arr.length - i; j++) {
+if (arr[j] > arr[j + 1]) {
+[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+}
+}
+}
+return console.log(arr)
+};
+stringSort(["banana", "apple", "cherry", "date"])
+*/
+
+/*
+let nestedArr=[[1, 5, 9], [2,[1,2,3,], 6], [3, 4, 7, 8]] ;
+let flatIt=(arr)=>{
+    return console.log(arr.flat(Infinity))
+};
+
+flatIt(nestedArr)
+*/
+
+
+
+
+
+
